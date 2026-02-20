5d13f99e651ff66eba7b211e35c7792d625e1958
k-runa <runakitayama00@gmail.com>
2026-02-20 10:11:07 +0900

    Update db.php with security settings

---
+++ includes/db.php
@@ -0,0 +1,25 @@
+
+
+<?php
+$databaseUrl = getenv('DATABASE_URL');
+
+if ($databaseUrl) {
+    $dbopts = parse_url($databaseUrl);
+    $dsn = "pgsql:host=" . $dbopts["host"] . ";port=" . $dbopts["port"] . ";dbname=" . ltrim($dbopts["path"], '/');
+    $user = $dbopts["user"];
+    $password = $dbopts["pass"] ?? '';
+} else {
+    $dsn = 'mysql:dbname=illustration;host=localhost;charset=utf8mb4';
+    $user = 'root';
+    $password = '';
+}
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo '接続失敗: ' . $e->getMessage();
+    exit;
+}
+?>
+
