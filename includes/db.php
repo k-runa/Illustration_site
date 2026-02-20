6bd596983bc07cf8e3db06a01810aefcc34eed96
k-runa <runakitayama00@gmail.com>
2026-02-20 10:55:48 +0900

    Fix: Final DSN format with port 5432

--- includes/db.php
+++ includes/db.php
@@ -2,14 +2,11 @@
 $databaseUrl = getenv('DATABASE_URL');
 
 if ($databaseUrl) {
-    // Render用：URLをそのまま使って接続する（一番確実な方法です）
-    $dsn = "pgsql:" . str_replace("postgresql://", "", $databaseUrl);
-    // ユーザー名とパスワードをURLから抜き出す
     $dbopts = parse_url($databaseUrl);
+    $dsn = "pgsql:host=" . $dbopts["host"] . ";port=5432;dbname=" . ltrim($dbopts["path"], '/');
     $user = $dbopts["user"];
     $password = $dbopts["pass"];
 } else {
-    // 自分のパソコン用
     $dsn = 'mysql:dbname=illustration;host=localhost;charset=utf8mb4';
     $user = 'root';
     $password = '';
